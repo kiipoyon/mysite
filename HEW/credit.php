9a2149655d14fc70e7f7e4fd044f9d12b9cfbc38
ishimaru-alt <51936419+ishimaru-alt@users.noreply.github.com>
2020-01-30 18:10:37 +0900

    Add files via upload

--- HEW/credit.php
+++ HEW/credit.php
@@ -20,6 +20,97 @@ require 'common/common.php';
 require 'model/Goods.php';
 require 'dao/GoodsDao.php';
 
+$flg=0;
+
+  	 //ログインボタンが押された場合
+  	 if(isset($_POST["loginbtn"])){
+
+            //画面入力のユーザーIDを取得する
+            $id=$_POST["id"];
+
+            //画面入力のパスワードを取得する
+            $password=$_POST["password"];
+
+  	     //ユーザIDの未入力チェック
+  	     if(empty($id)){
+  	         echo "メールアドレスが未入力です";
+  	     }elseif(empty($password)){
+  	         //パスワードの未入力チェック
+  	         echo "パスワードが未入力です";
+  	     }
+       }
+       
+       if (isset ($_SESSION['roginid'])) {
+        $id = $_SESSION['roginid'];
+        //画面入力のパスワードを取得する
+        $password=$_SESSION["password"];
+      }
+
+
+
+
+
+  	 //IDとパスワードが一致しているか確認する
+  	 if(!empty($id) && !empty($password)){
+
+        
+            //データベースに接続する
+            $pdo=new PDO('mysql:host=localhost;dbname=haldb;charset=utf8','dbadmin','dbadmin');
+
+            //ログイン情報を検索し、検索結果をステートメントに設定する($loginidはPOSTで持ってきたもの) ここをprepareにする
+            $st=$pdo->prepare("SELECT * FROM user_tbl WHERE user_id=?");
+
+            //$id = $_POST['id']; // ユーザーIDをセッション変数にセット
+
+            //bindValueメソッドでパラメータをセット
+            $st->bindValue(1,$password);
+
+            //executeでクエリを実行
+            $st->execute();
+
+            //処理結果を配列logininfoに設定する loginidが主キーならこの処理はいらない
+            $logininfo=$st->fetch();
+
+            //ログイン成功フラグを初期化する（ログイン成功フラグ＝０にする）
+            $flg=0;
+            //パスワードが一致しているかどうかチェックする
+            //foreach($logininfo as $login){
+            //ログイン情報のパスワードと画面d入力したパスワードが一致しているか比較する
+
+
+
+
+
+            if(password_verify($password, $logininfo['password'])){
+            print '認証成功';
+            $flg=1;
+            session_regenerate_id(true); // セッションIDをふりなおす
+            $_SESSION['roginid'] = $id; // ユーザーIDをセッション変数にセット
+            $_SESSION['password'] = $password;
+            }else{
+            print '認証成功しない';
+            }
+
+          //}
+
+
+          $st2=$pdo->prepare("select name from user_details_tbl where user_id=?");
+          //bindValueメソッドでパラメータをセット
+          $st2->bindValue(1,$id);
+
+          //executeでクエリを実行
+          $st2->execute();
+
+          $logininfo2=$st2->fetchAll();
+
+          foreach($logininfo2 as $login2){
+              $name=$login2['name'];
+          }
+
+
+      }
+
+
 
 $gggg = $_SESSION['cart'];
 $product_id = $_SESSION['code'];
@@ -29,7 +120,6 @@ $product_id = $_SESSION['code'];
 
 
 
-// カート内商品点数分繰り返す
 foreach($_SESSION['cart'] as $code => $num) {
     // 商品テーブルを検索する
     $goods_dao = new GoodsDao();
@@ -42,6 +132,11 @@ foreach($_SESSION['cart'] as $code => $num) {
     $sum += $num * $goods->getPrice();
     $rows[] = $goods;
     }
+
+//再送信のエラーを消す
+header('Expires: -1');
+header('Cache-Control:');
+header('Pragma:');
 
 // 最初の画面を表示する
 require 'view/creditView.php';
