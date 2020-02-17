8c29cd2ffb7db1700fc7dcb0ee1d4096591fc22f
ishimaru-alt <51936419+ishimaru-alt@users.noreply.github.com>
2020-02-17 09:47:46 +0900

    Add files via upload

--- HEW/view/listView.php
+++ HEW/view/listView.php
@@ -74,6 +74,34 @@
         <a href="list.php?idd=5#abc" class="area" method="GET">関西</a>
         <a href="list.php?idd=6#abc" class="area" method="GET">中国・四国</a>
         <a href="list.php?idd=7#abc" class="area" method="GET">九州・沖縄</a>
+        <form name="form1" method="POST" action="list.php" >
+        <a href="javascript:document.form1.submit()">北海道</A>
+        <input type="hidden" name="hogehoge_status" value="1">
+        </form>
+        <form name="form2" method="POST" action="list.php" >
+        <a href="javascript:document.form2.submit()">東北</A>
+        <input type="hidden" name="hogehoge_status" value="2">
+        </form>
+        <form name="form3" method="POST" action="list.php" >
+        <a href="javascript:document.form3.submit()">関東</A>
+        <input type="hidden" name="hogehoge_status" value="3">
+        </form>
+        <form name="form4" method="POST" action="list.php" >
+        <a href="javascript:document.form4.submit()">中部</A>
+        <input type="hidden" name="hogehoge_status" value="4">
+        </form>
+        <form name="form5" method="POST" action="list.php" >
+        <a href="javascript:document.form5.submit()">関西</A>
+        <input type="hidden" name="hogehoge_status" value="5">
+        </form>
+        <form name="form6" method="POST" action="list.php" >
+        <a href="javascript:document.form6.submit()">中国・四国</A>
+        <input type="hidden" name="hogehoge_status" value="6">
+        </form>
+        <form name="form7" method="POST" action="list.php" >
+        <a href="javascript:document.form7.submit()">九州・沖縄</A>
+        <input type="hidden" name="hogehoge_status" value="7">
+        </form>
       </div>
       <p class="area_title">
         <i class="material-icons miCSS">search</i>
@@ -86,6 +114,34 @@
       <a href="list.php?categoly=4#abc" class="area" method="GET">乳製品</a>
       <a href="list.php?categoly=5#abc" class="area" method="GET">果物</a>
       <a href="list.php?categoly=6#abc" class="area" method="GET">日本酒・ワイン・酒</a>
+        <form name="cateform1" method="POST" action="list.php" >
+        <a href="javascript:document.cateform1.submit()">海鮮・水産加工品</A>
+        <input type="hidden" name="categoly" value="1">
+        </form>
+        <form name="cateform2" method="POST" action="list.php" >
+        <a href="javascript:document.cateform2.submit()">肉・ハム</A>
+        <input type="hidden" name="categoly" value="2">
+        </form>
+        <form name="cateform3" method="POST" action="list.php" >
+        <a href="javascript:document.cateform3.submit()">野菜</A>
+        <input type="hidden" name="categoly" value="3">
+        </form>
+        <form name="cateform4" method="POST" action="list.php" >
+        <a href="javascript:document.cateform4.submit()">乳製品</A>
+        <input type="hidden" name="categoly" value="4">
+        </form>
+        <form name="cateform5" method="POST" action="list.php" >
+        <a href="javascript:document.cateform5.submit()">果物</A>
+        <input type="hidden" name="categoly" value="5">
+        </form>
+        <form name="cateform6" method="POST" action="list.php" >
+        <a href="javascript:document.cateform6.submit()">日本酒・ワイン・酒</A>
+        <input type="hidden" name="categoly" value="6">
+        </form>
+        <form name="cateform7" method="POST" action="list.php" >
+        <a href="javascript:document.cateform7.submit()">加工品</A>
+        <input type="hidden" name="categoly" value="7">
+        </form>
       <p class="area_title">
         <i class="material-icons miCSS">search</i>
         価格
@@ -94,16 +150,15 @@
       <div class="cost">
         <p>価格（税込）を指定</p>
         <form action="list.php" method="post">
-        <input type="text" name="mini" class="cost_t">～<input type="text" name="max" class="cost_t">円
-
-            <input type="submit" name="submit" value="絞り込み" class="btn-square">
-          </form>
+          <input type="text" name="mini" class="cost_t">～<input type="text" name="max" class="cost_t">円
+          <input type="submit" name="submit" value="絞り込み" class="btn-square">
+        </form>
       </div>
   </div>
 
 <!-- メインコンテンツ -->
   <div class="main_flex2">
-    <h1 id="abc"><?php echo $area?></h1>
+
     <!-- 並べ替え -->
     <div class="cp_ipselect cp_sl02">
       <form action="list.php" method="post">
@@ -115,6 +170,20 @@
         </select>
       </form>
     </div>
+    <div>
+    <p>
+            <?php
+              //ページネーションを表示
+                  for ( $n = 1; $n <= $pages; $n ++){
+                      if ( $n == $now ){
+                          echo "<span style='padding: 5px;'>$now</span>";
+                      }else{
+                          echo "<a href='list.php?page_id=$n' style='padding: 5px;'>$n</a>";
+                      }
+                  }
+              ?>
+              </p>
+    </div>
     <div class="syouhin">
     <?php
         /* 一覧を３つ並べて表示 */
@@ -122,7 +191,7 @@
         $col = 3; // カラム数
     ?>
         <table>
-            <?php foreach ($product_tbl as $g) { ?>
+            <?php foreach ($data as $g) { ?>
               <?php if ($idx % $col == 0) { ?>
               <tr>
               <?php } ?>
@@ -144,13 +213,11 @@
               </tr>
             <?php } ?>
         </table>
-        <?php
-          $count=$sta->rowCount();
-          if($count == 0){
-              $declaration = "該当なし";
-              print $declaration;
-          }
-        ?>
+
+
+
+
+
   </div>
 </div>
 </main>
